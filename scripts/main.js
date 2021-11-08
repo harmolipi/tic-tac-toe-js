2e082c7efb4a95351e56dc8b9359d8972c8098c8
harmolipi <nikob381@gmail.com>
2021-11-08 17:05:47 -0500

    Added displayController and players

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,24 @@ const gameBoard = (() => {
   ];
   return board;
 })();
+
+const displayController = (() => {
+  const updateDisplay = (board) => {
+    gameBoard.forEach((row, rowIndex) => {
+      row.forEach((cell, cellIndex) => {
+        const cellElement = document.querySelector(`[data-row="${rowIndex}"][data-cell="${cellIndex}"]`);
+        cellElement.innerText = cell;
+        cellElement.classList.add(cellElement.innerText == 'X' ? 'red' : 'blue');
+      });
+    });
+  };
+  return { updateDisplay };
+})();
+
+const player = (marker) => {
+  return { marker };
+}
+
+const player1 = player('X');
+const player2 = player('O');
+
